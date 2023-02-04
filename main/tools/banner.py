d40085f923685215403e87db5b658ebe6304bdc8
Hardik Kasliwal <64286654+THECH13F@users.noreply.github.com>
2023-02-05 01:12:51 +0530

    Update banner.py

--- main/tools/banner.py
+++ main/tools/banner.py
@@ -16,7 +16,19 @@ def main():
 def attack(name):
     print(f"\u001b[32m\t\t\t\t{name}")
     print("\u001b[37m--------------------------------------------------------------------------------")
+def wrap_text(text, width=80):
+    wrapped_text = []
+    lines = text.split(" ")
+    line = ""
+    for word in lines:
+        if len(line) + len(word) <= width:
+            line += word + " "
+        else:
+            wrapped_text.append(line)
+            line = word + " "
+    wrapped_text.append(line)
+    return "\n".join(wrapped_text)
 def description(Description):
     print("\u001b[34mDescription:")
-    print(Description)
+    print(wrap_text(Description))
     print("\u001b[37m--------------------------------------------------------------------------------")
