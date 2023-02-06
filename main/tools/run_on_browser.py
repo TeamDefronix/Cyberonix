c49ae921f579cd0d0a9c7ce705bdfe6ec43a4c28
Hardik Kasliwal <hardikkasli123@gmail.com>
2023-02-06 21:59:37 +0530

    Update run_on_browser.py

--- main/tools/run_on_browser.py
+++ main/tools/run_on_browser.py
@@ -5,9 +5,9 @@ def main(URL):
         print("[+] Opening Article")
         user=os.getlogin()
         if "root" in user:
-            os.system(f"firefox {URL} /dev/null 2>&1" )
+            os.system(f"firefox {URL} > /dev/null 2>&1" )
         else:
             os.system(f"sudo chown root:root /run/user/{user}/gdm/Xauthority > /dev/null 2>&1")
             # os.system(f"firefox {URL} 2>/dev/null")
             os.system(f"sudo chown root:root /home/{user}/.Xauthority > /dev/null 2>&1")
-            os.system(f"firefox {URL} /dev/null 2>&1")
+            os.system(f"firefox {URL} > /dev/null 2>&1")
