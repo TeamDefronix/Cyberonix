08b8a9107c0c425acfc62e277a54ad40ebbe80f5
Hardik Kasliwal <hardikkasli123@gmail.com>
2023-02-06 21:56:08 +0530

    fixed

--- main/tools/run_on_browser.py
+++ main/tools/run_on_browser.py
@@ -3,23 +3,11 @@ import os
 def main(URL):
         # print("[+] Opening url")
         print("[+] Opening Article")
-        proc = subprocess.Popen([f"pwd"], stdout=subprocess.PIPE, shell=True)
-        #there keyfor success output and noththere for error output
-        (there, notthere) = proc.communicate()
-        there=there.decode()
-        there=there.split("/")
-        if "root" in there:
+        user=os.getlogin()
+        if "root" in user:
             os.system(f"firefox {URL} 2>/dev/null" )
         else:
-            #this is to get desktop enviroment
-            proc = subprocess.Popen([f"echo $DESKTOP_SESSION"], stdout=subprocess.PIPE, shell=True)
-            (envo, noenvo) = proc.communicate()
-            #this is to get username
-            proc = subprocess.Popen([f"cat /etc/passwd | grep {there[2]}"], stdout=subprocess.PIPE, shell=True)
-            (uid, notuid) = proc.communicate()
-            uid=uid.decode()
-            uid=uid.split(":")
-            os.system(f"sudo chown root:root /run/user/{uid[2]}/gdm/Xauthority > /dev/null 2>&1")
+            os.system(f"sudo chown root:root /run/user/{user}/gdm/Xauthority > /dev/null 2>&1")
             # os.system(f"firefox {URL} 2>/dev/null")
-            os.system(f"sudo chown root:root /home/{there[2]}/.Xauthority > /dev/null 2>&1")
+            os.system(f"sudo chown root:root /home/{user}/.Xauthority > /dev/null 2>&1")
             os.system(f"firefox {URL} 2>/dev/null")
